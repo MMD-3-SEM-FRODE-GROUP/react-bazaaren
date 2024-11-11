3a729f684189bd973185ea141f253a6a1ca00e5e
Ester <espi0001@stud.kea.dk>
2024-11-11 15:57:37 +0100

    produkter

--- src/app/produkter/page.js
+++ src/app/produkter/page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import AlleProdukter from "@/components/AlleProdukter";
 
 let response = await fetch("https://dummyjson.com/products");
 let data = await response.json();
@@ -7,11 +8,20 @@ let data = await response.json();
 const Produkter = () => {
   return (
     <section>
-      <div className="grid grid-cols-3 gap-10">
+      <article className="flex justify-between items-center">
+        <div>
+          <h1 className="text-[3.6rem] font-bold">Produkter</h1>
+          <p>lorem ipsum</p>
+        </div>
+        <button>Se alle</button>
+      </article>
+
+      <div className="grid grid-cols-4 gap-x-[2rem] gap-y-[4rem]">
         {data.products.map((product) => (
           <div key={product.id}>
             <Image src={product.thumbnail} width={250} height={250} alt={product.title} />
             <Link href={`/detaljer/${product.id}`}>{product.title}</Link>
+            <p>{product.price} kr</p>
           </div>
         ))}
       </div>
