b79c0ae10f640fd70eee0b1b756996bb47daa501
Ester <espi0001@stud.kea.dk>
2024-11-11 15:31:50 +0100

    forside

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,19 @@ import Image from "next/image";
 
 export default async function Home() {
   return (
-    <div>
-      <h1>Lorem Ipsum</h1>
-      <button>
-        <Link href="/produkter">Se Produkter</Link>
-      </button>
-    </div>
+    <section className="bg-black bg-cover bg-center width-[100vw]">
+      <div className="text-center p-[200px] text-white flex flex-col place-items-center">
+        <h1 className="text-[3.6rem] font-bold">بقخيث BAZAAR</h1>
+        <p className="text-center	max-w-[768px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
+        <div className="flex gap-[20px] mt-3">
+          <button>
+            <Link href="/produkter">Se Produkter</Link>
+          </button>
+          <button>
+            <Link href="/produkter">Se Produkter</Link>
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
